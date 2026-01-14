ff014153b425664d55d334997598b12b6fdf042a
Rudy <idejrdud@gmail.com>
2026-01-14 14:28:07 +0900

    first commit

---
+++ src/main/java/kr/it/rudy/blog/RudyBlogApplication.java
@@ -0,0 +1,13 @@
+package kr.it.rudy.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RudyBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RudyBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/it/rudy/blog/RudyBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.it.rudy.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RudyBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
